7a01423cf46523eff97c1ea4964e0683e9b50dd8
ryanfinken <ryan.finken@daugherty.com>
2022-06-06 10:00:56 -0500

    finished prime numbers and bowling katas

--- src/testsFirst.js
+++ src/testsFirst.js
@@ -1,21 +1,41 @@
 export const findAllPrimeFactors = (number) => {
-    const output = [];
+    const output = []
     while(number % 2 === 0) {
-        output.push(2);
-        number = number / 2;
+        output.push(2)
+        number = number / 2
     }
 
     for(let i = 3; i <= Math.sqrt(number); i += 2) {
         while (number % i === 0) {
             output.push(i);
-            number = number / i;
+            number = number / i
         }
     }
 
     if (number > 2) {
-        output.push(number);
+        output.push(number)
     }
 
-    return output;
+    return output
+}
+
+export const scoreBowlingGame = (gameFrames) => {
+    let score = 0
+
+    for (let i = 0; i < 10; i++) {
+        score += gameFrames[i][0] + gameFrames[i][1];
+        if (gameFrames[i][0] === 10) {
+            score += gameFrames[i + 1][0]
+            if (gameFrames[i + 1][0] === 10) {
+                score += gameFrames[i + 2][0]
+            } else {
+                score += gameFrames[i + 1][1]
+            }
+        } else if (gameFrames[i][0] + gameFrames[i][1] === 10) {
+            score += gameFrames[i + 1][0]
+        }
+    }
+
+    return score;
 }
 
--- src/testsFirst.test.js
+++ src/testsFirst.test.js
@@ -1,5 +1,6 @@
-import {findAllPrimeFactors} from './testsFirst'
+import {findAllPrimeFactors, scoreBowlingGame} from './testsFirst'
 
+// unit tests for findAllPrimeFactors
 it('should return [2]', () => {
     const actual = findAllPrimeFactors(2);
     expect(actual).toEqual([2]);
@@ -28,4 +29,55 @@ it('should return [3, 3]', () => {
 it('should return [2, 2, 2, 137]', () => {
     const actual = findAllPrimeFactors(1096)
     expect(actual).toEqual([2, 2, 2, 137])
+})
+
+// unit tests for scoreBowlingGame
+it('should return a score of 0', () => {
+    const actual = scoreBowlingGame([[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])
+    expect(actual).toEqual(0)
+})
+
+it('should return a score of 1', () => {
+    const actual = scoreBowlingGame([[1, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])
+    expect(actual).toEqual(1);
+})
+
+it('should return a score of 2', () => {
+    const actual = scoreBowlingGame([[1, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])
+    expect(actual).toEqual(2);
+})
+
+it('should return a score of 3', () => {
+    const actual = scoreBowlingGame([[1, 1], [1, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])
+    expect(actual).toEqual(3);
+})
+
+it('should return a score of 4', () => {
+    const actual = scoreBowlingGame([[1, 1], [1, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])
+    expect(actual).toEqual(4);
+})
+
+it('should return a score of 12', () => {
+    const actual = scoreBowlingGame([[5, 5], [1, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])
+    expect(actual).toEqual(12);
+})
+
+it('should return a score of 11', () => {
+    const actual = scoreBowlingGame([[5, 5], [0, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])
+    expect(actual).toEqual(11);
+})
+
+it('should return a score of 14', () => {
+    const actual = scoreBowlingGame([[10, 0], [1, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])
+    expect(actual).toEqual(14);
+})
+
+it('should return a score of 33', () => {
+    const actual = scoreBowlingGame([[10, 0], [10, 0], [1, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])
+    expect(actual).toEqual(33);
+})
+
+it('should return a score of 300 for a perfect game', () => {
+    const actual = scoreBowlingGame([[10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0]])
+    expect(actual).toEqual(300);
 })
